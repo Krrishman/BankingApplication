90932b07883583aefcdf98cad83a6be5ece7f250
Andor-117 <Newo3232@gmail.com>
2022-04-27 09:06:13 -0400

    Pushing Account Interface

---
+++ src/main/java/AccountInterface.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author Owner
+ */
+public interface AccountInterface {
+    
+    int balance = 0;
+    
+    public int showBalance();
+    
+    public void deposit();
+    
+    public void withdraw();
+}
